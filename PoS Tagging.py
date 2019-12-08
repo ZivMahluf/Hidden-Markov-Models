403514dcf27e8a898f1b59a75f14943ac2d8b530
Ziv Mahluf <ziv5467@gmail.com>
2019-12-08 11:11:40 +0200

    Final version, includes the implementation of Baseline, HMM and MEMM.

--- PoS Tagging.py
+++ PoS Tagging.py
@@ -1,8 +1,6 @@
 import pickle
 from collections import defaultdict
-import time  # TODO: for testing
 from sklearn.model_selection import train_test_split
-
 import numpy as np
 
 START_STATE = '*START*'
@@ -10,6 +8,9 @@ START_WORD = '*START*'
 END_STATE = '*END*'
 END_WORD = '*END*'
 RARE_WORD = '*RARE_WORD*'
+
+EPSILON = 0.00000001
+BATCH_SIZE = 5
 
 
 def data_example(data_path='PoS_data.pickle',
@@ -156,8 +157,10 @@ class HMM(object):
         self.pos_size = len(pos_tags)
         self.pos2i = {pos: i for (i, pos) in enumerate(pos_tags)}
         self.word2i = {word: i for (i, word) in enumerate(words)}
+        self.i2pos = {i: pos for (i, pos) in enumerate(pos_tags)}
+        self.i2word = {i: word for (i, word) in enumerate(words)}
 
-        # pos_probs is |pos|*|pos|, and emissions is size |pos|*|words|
+        # transitions is |pos|*|pos|, and emissions is size |pos|*|words|
         self.transition, self.emissions = hmm_mle(training_set, self)
 
     def sample(self, n):
@@ -165,8 +168,23 @@ class HMM(object):
         Sample n sequences of words from the HMM.
         :return: A list of word sequences.
         '''
-
-        # TODO: YOUR CODE HERE
+        sequences = []
+        for i in range(n):
+            current_pos = START_STATE
+            seq = [START_WORD]
+            while current_pos != END_STATE:
+                current_dict = self.transition[current_pos]
+                current_values = list(current_dict.values())
+                current_keys = list(current_dict.keys())
+                # randomly pick the next pos from the current pos options, by the probs of the transition matrix
+                next_pos_index = np.random.choice(len(current_dict), 1, p=current_values)[0]
+                next_pos = current_keys[next_pos_index]
+                next_pos_emission_words = list(self.emissions[next_pos].keys())
+                next_word = np.random.choice(next_pos_emission_words, 1)[0]
+                seq.append(next_word)
+                current_pos = next_pos
+            sequences.append(seq)
+        return sequences
 
     def viterbi(self, sentences):
         '''
@@ -175,9 +193,7 @@ class HMM(object):
         :param sentences: iterable sequence of word sequences (sentences).
         :return: iterable sequence of PoS tag sequences.
         '''
-        # TODO: USE LOG to avoid numerical issues
         pos_predictions = []  # list of lists of pos
-
         for sentence in sentences:
             sentence_pos_prediction = []
             dynamic_matrix = {k: defaultdict(int) for k in range(len(sentence))}  # size |sentence| * |pos| * |2|
@@ -193,8 +209,8 @@ class HMM(object):
                 for u in self.pos_tags:
                     # find w which maximizes the equation
                     max_val, max_w = 0.0, START_STATE
-                    for index, w in enumerate(self.pos_tags):
-                        value = dynamic_matrix[k-1][w][0] * self.transition[u][w] * self.emissions[u][word]
+                    for w in self.pos_tags:
+                        value = dynamic_matrix[k - 1][w][0] * self.transition[w][u] * self.emissions[u][word]
                         if value > max_val:
                             max_val, max_w = value, w
                     dynamic_matrix[k][u] = (max_val, max_w)
@@ -209,7 +225,7 @@ class HMM(object):
             for k in range(len(sentence) - 1, -1, -1):  # iterate backwards
                 sentence_pos_prediction.append(max_pos)
                 _, max_pos = dynamic_matrix[k][max_pos]
-            sentence_pos_prediction.reverse()  # because we added backwards
+            sentence_pos_prediction.reverse()  # because we appended backwards
             pos_predictions.append(sentence_pos_prediction)
         return pos_predictions
 
@@ -226,42 +242,30 @@ def hmm_mle(training_set, model):
             the probabilities in |PoS|x|PoS| and |PoS|x|Words| sized matrices, or
             any other data structure you prefer.
     """
-    start_time = time.time()
-
     # get info from data
     X = [training_set[i][1] for i in range(len(training_set))]
     y = [training_set[i][0] for i in range(len(training_set))]
 
     # get pos tags appearance rate
     pos_appearance = defaultdict(int)
-    for pos_labels in y:
-        for pos in pos_labels:
+    for sentence_pos in y:
+        for pos in sentence_pos:
             pos_appearance[pos] += 1
 
-    # get matrix of pos appearance (matrix to calculate P[y_(k-1) = specific_pos])
-    pos_appearance_at_k = {pos: defaultdict(int) for pos in model.pos_tags}  # size ~ |pos| * |max length sentence|
+    # transition matrix
+    transitions = {pos: defaultdict(float) for pos in model.pos_tags}
     for sentence_pos in y:
-        for t in range(len(sentence_pos)):
-            pos_appearance_at_k[sentence_pos[t]][t] += 1 / len(y)
+        for k in range(len(sentence_pos) - 1):
+            transitions[sentence_pos[k]][sentence_pos[k + 1]] += 1 / pos_appearance[sentence_pos[k]]
 
-    # get pos probabilities matrix (transition matrix, P(y_k| y_(k-1) ) - by calculating P(yk, y_(k-1)) / P(y_(k-1))
-    transition_matrix = {pos: defaultdict(int) for pos in model.pos_tags}
-    for sentence_pos in y:
-        for k in range(1, len(sentence_pos)):
-            transition_matrix[sentence_pos[k]][sentence_pos[k-1]] += 1 / (pos_appearance_at_k[sentence_pos[k-1]][k-1])
-
-    pos_by_word = {pos: defaultdict(int) for pos in model.pos_tags}
+    emissions = {pos: defaultdict(int) for pos in model.pos_tags}
     # get P(x|y) by getting all words for each pos
-    for i in range(len(X)):
-        sentence = X[i]
-        pos_of_sentence = y[i]
+    for pos_of_sentence, sentence in training_set:
         for j in range(len(sentence)):  # iterate over each word and it's corresponding pos
             # we divide by the amount of words of the pos
-            pos_by_word[pos_of_sentence[j]][sentence[j]] += 1 / pos_appearance[pos_of_sentence[j]]
+            emissions[pos_of_sentence[j]][sentence[j]] += 1 / pos_appearance[pos_of_sentence[j]]
 
-    end_time = time.time()
-    print("hmm mle time taken:%0.4f seconds" % (end_time - start_time))
-    return transition_matrix, pos_by_word
+    return transitions, emissions
 
 
 class MEMM(object):
@@ -279,7 +283,6 @@ class MEMM(object):
                     and a word, and returns a list of indices that have a "1" in
                     the binary feature vector.
         '''
-
         self.words = words
         self.pos_tags = pos_tags
         self.words_size = len(words)
@@ -287,8 +290,9 @@ class MEMM(object):
         self.pos2i = {pos: i for (i, pos) in enumerate(pos_tags)}
         self.word2i = {word: i for (i, word) in enumerate(words)}
         self.phi = phi
+        self.i2pos = {i: pos for (i, pos) in enumerate(pos_tags)}
 
-        # TODO: YOUR CODE HERE
+        self.phi2_vector_size = self.pos_size * self.pos_size + self.pos_size * self.words_size + 3
 
     def viterbi(self, sentences, w):
         '''
@@ -298,8 +302,62 @@ class MEMM(object):
         :param w: a dictionary that maps a feature index to it's weight.
         :return: iterable sequence of POS tag sequences.
         '''
+        pos_predictions = []
+        for sentence in sentences:
+            k = len(sentence)
+            dynamic_matrix = np.zeros((k, self.pos_size, 2))
+            pos_pred = []
+            # init the matrix by using START as the first pos
+            for j in range(self.pos_size):
+                dynamic_matrix[0, j, 0] = np.sum(w[self.phi(self, sentence[0], self.i2pos[j], START_STATE)])
 
-        # TODO: YOUR CODE HERE
+            for i in range(1, k):
+                if sentence[i] not in self.words:
+                    sentence[i] = RARE_WORD
+                for j in range(self.pos_size):
+                    temp = np.zeros(self.pos_size)
+                    for t in range(self.pos_size):
+                        temp[t] = np.sum(w[self.phi(self, sentence[i], self.i2pos[j], self.i2pos[t])])
+
+                    temp += dynamic_matrix[i - 1, :, 0]
+                    dynamic_matrix[i, j, 0] = np.max(temp)
+                    dynamic_matrix[i, j, 1] = np.argmax(temp)
+
+            pos_idx = np.argmax(dynamic_matrix[-1, :, 0])
+            for i in range(k - 1, -1, -1):  # run backwards
+                pos_pred.append(self.i2pos[pos_idx])
+                next_pos_idx = dynamic_matrix[i, pos_idx, 1]
+                pos_idx = int(next_pos_idx)
+
+            pos_pred.reverse()
+            pos_predictions.append(pos_pred)
+
+        return pos_predictions
+
+    def phi_func(self, word, y_new, y_old):
+        index_word = self.word2i[word]
+        index_y_new = self.pos2i[y_new]
+        index_y_old = self.pos2i[y_old]
+        # index at transitions matrix, and index at emissions matrix that comes after the transition matrix
+        return np.array([index_y_old * self.pos_size + index_y_new,
+                         self.pos_size * self.pos_size + index_word * self.pos_size + index_y_new])
+
+    def phi_func2(self, word, y_new, y_old):
+        index_word = self.word2i[word]
+        index_y_new = self.pos2i[y_new]
+        index_y_old = self.pos2i[y_old]
+        indices = [index_y_old * self.pos_size + index_y_new,
+                   self.pos_size * self.pos_size + index_word * self.pos_size + index_y_new]
+
+        # add index of features
+        if word.isupper():
+            indices.append(self.phi2_vector_size + 1)
+        if word.isdigit():
+            indices.append(self.phi2_vector_size + 2)
+        if len(word) < 6:
+            indices.append(self.phi2_vector_size + 3)
+
+        return np.array(indices)
 
 
 def perceptron(training_set, initial_model, w0, eta=0.1, epochs=1):
@@ -313,8 +371,15 @@ def perceptron(training_set, initial_model, w0, eta=0.1, epochs=1):
     :param epochs: the amount of times to go over the entire training data (default is 1).
     :return: w, the learned weights vector for the MEMM.
     """
-
-    # TODO: YOUR CODE HERE
+    for epoch in range(epochs):
+        for pos, sentence in training_set:
+            pos_pred = initial_model.viterbi([sentence], w0)[0]
+            w0[initial_model.phi(initial_model, sentence[0], pos[0], START_STATE)] += eta
+            w0[initial_model.phi(initial_model, sentence[0], pos_pred[0], START_STATE)] -= eta
+            for j in range(1, len(sentence)):
+                w0[initial_model.phi(initial_model, sentence[j], pos[j], pos[j - 1])] += eta
+                w0[initial_model.phi(initial_model, sentence[j], pos_pred[j], pos_pred[j - 1])] -= eta
+    return w0
 
 
 # --------------- Added functions ---------------
@@ -341,7 +406,7 @@ def preprocess_data(data, words, pos):
     words.append(END_WORD)
     words = set(words)
     # append start and end
-    pos.append(START_STATE)
+    pos.insert(0, START_STATE)
     pos.append(END_STATE)
     # add words and states
     for i in range(len(data)):
@@ -379,19 +444,62 @@ def get_word_appearance_rate(X, words, count_total_amount=False):
 
 
 if __name__ == '__main__':
+    # for MEMM tests:
+
     data, words, pos = get_data()
     data, words, pos = preprocess_data(data, words, pos)
     # get X and y
     X = [data[i][1] for i in range(len(data))]
     y = [data[i][0] for i in range(len(data))]
     # split data
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.9, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
-    # # TESTS -- HMM:SMALL DATA
-    # small_test_samples = 100
-    # small_set = [(y_train[i], x_train[i]) for i in range(small_test_samples)]
-    # small_x_test = x_test[:1000]
-    # small_y_test = y_test[:1000]
+    # TESTS -- sample in HMM
+    small_train_samples = len(x_train)
+    small_test_samples = 10
+    small_set = [(y_train[i], x_train[i]) for i in range(small_train_samples)]
+    small_x_test = x_test[:small_test_samples]
+    small_y_test = y_test[:small_test_samples]
+    words_in_small_set = {word for i in range(len(small_set)) for word in small_set[i][1]}
+    hmm_model = HMM(pos, words_in_small_set, small_set)
+    sampled_sentences = hmm_model.sample(5)
+    for sentence in sampled_sentences:
+        print(sentence)
+
+    # # TESTS -- MEMM
+    # start = time.time()
+    # small_train_samples = len(x_train)
+    # small_test_samples = len(x_train)
+    # small_set = [(y_train[i], x_train[i]) for i in range(small_train_samples)]
+    # small_x_test = x_test[:small_test_samples]
+    # small_y_test = y_test[:small_test_samples]
+    # words_in_small_set = {word for i in range(len(small_set)) for word in small_set[i][1]}
+    #
+    # memm_model = MEMM(pos, words_in_small_set, small_set, MEMM.phi_func)
+    # size_of_transitions_and_emissions = memm_model.pos_size * memm_model.pos_size + \
+    #                                     memm_model.words_size * memm_model.pos_size
+    # w0 = np.zeros(size_of_transitions_and_emissions)  # init w0
+    # w = perceptron(small_set, memm_model, w0)
+    # print("finished perceptron")
+    # pos_predictions = memm_model.viterbi(small_x_test, w)
+    #
+    # avg_acc = 0
+    # for sample_index in range(len(small_y_test)):
+    #     acc = 0
+    #     for pos_index in range(len(small_y_test[sample_index])):
+    #         if small_y_test[sample_index][pos_index] == pos_predictions[sample_index][pos_index]:
+    #             acc += 1
+    #     avg_acc += acc / len(small_y_test[sample_index])
+    # print("accuracy:" + str(avg_acc / len(small_y_test)))
+    # end = time.time()
+    # print("total time for memm on 100 test samples: %0.4f seconds" % (end - start))
+
+    # # TESTS -- HMM
+    # small_train_samples = len(x_train)
+    # small_test_samples = int(len(x_train) / 9)
+    # small_set = [(y_train[i], x_train[i]) for i in range(small_train_samples)]
+    # small_x_test = x_test[:small_test_samples]
+    # small_y_test = y_test[:small_test_samples]
     # words_in_small_set = {word for i in range(len(small_set)) for word in small_set[i][1]}
     # hmm_model = HMM(pos, words_in_small_set, small_set)
     # pos_predictions = hmm_model.viterbi(small_x_test)
@@ -405,22 +513,23 @@ if __name__ == '__main__':
     #     avg_acc += acc / len(small_y_test[sample_index])
     # print("accuracy:" + str(avg_acc / len(small_y_test)))
 
-    # small_set = data[:10000]
+    # # TESTS -- BASELINE
+    # small_train_samples = len(x_train)
+    # small_test_samples = int(len(x_test))
+    # small_set = [(y_train[i], x_train[i]) for i in range(small_train_samples)]
+    # small_x_test = x_test[:small_test_samples]
+    # small_y_test = y_test[:small_test_samples]
     # words_in_small_set = {word for i in range(len(small_set)) for word in small_set[i][1]}
+    #
     # baseline_model = Baseline(pos, words_in_small_set, small_set)
-
-    # # TESTS -- BASELINE:ALL DATA
-    # new_data = [(y_train[i], x_train[i]) for i in range(len(x_train))]
-    # words_in_new_data = {word for i in range(len(x_train)) for word in x_train[i]}
-    # baseline_model = Baseline(pos, words_in_new_data, new_data)
-    # pos_predictions = baseline_model.MAP(x_test)
+    # pos_predictions = baseline_model.MAP(small_x_test)
     #
     # avg_acc = 0
-    # for sample_index in range(len(y_test)):
+    # for sample_index in range(len(small_y_test)):
     #     acc = 0
-    #     for pos_index in range(len(y_test[sample_index])):
-    #         if y_test[sample_index][pos_index] == pos_predictions[sample_index][pos_index]:
+    #     for pos_index in range(len(small_y_test[sample_index])):
+    #         if small_y_test[sample_index][pos_index] == pos_predictions[sample_index][pos_index]:
     #             acc += 1
-    #     avg_acc += acc / len(y_test[sample_index])
-    # print("accuracy:" + str(avg_acc / len(y_test)))
+    #     avg_acc += acc / len(small_y_test[sample_index])
+    # print("accuracy:" + str(avg_acc / len(small_y_test)))
     # print("finished")
